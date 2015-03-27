68b7b173894be073ae0828a5e83f5a795c971fd0
Luca Heltai <luca.heltai@sissa.it>
2015-03-27 23:05:23 +0100

    Moved function "type" to utilities.h

--- include/utilities.h
+++ include/utilities.h
@@ -3,9 +3,18 @@
 
 #include <deal.II/base/utilities.h>
 #include <deal.II/base/smartpointer.h>
+#include <typeinfo>
+#include <cxxabi.h>
 
 using namespace dealii;
 
+
+/**
+ * SmartPointers are usually not used to point to objects created with
+ * new. However, sometimes this is useful. The distruction of a
+ * SmartPointer requires to split the step in two parts. This little
+ * utility does precisely this.
+ */
 template <typename TYPE>
 void smart_delete (SmartPointer<TYPE> &sp) {
   if(sp) {
@@ -14,5 +23,29 @@ void smart_delete (SmartPointer<TYPE> &sp) {
     delete p;
   }
 }
+
+// Anonymous namespace, to hide implementation detail for the type
+// function below
+namespace {
+  struct handle {
+    char* p;
+    handle(char* ptr) : p(ptr) { }
+    ~handle() { std::free(p); }
+  };
+  
+  std::string demangle(const char* name) {
+    int status = -4; // some arbitrary value to eliminate the compiler warning
+    handle result( abi::__cxa_demangle(name, NULL, NULL, &status) );
+    return (status==0) ? result.p : name ;
+  };
+}
+
+/**
+ * Return a human readable name of the type passed as argument.
+ */
+template <class T>
+std::string type(const T& t) {
+  return demangle(typeid(t).name());
+};
 
 #endif
--- source/parameter_acceptor.cc
+++ source/parameter_acceptor.cc
@@ -1,26 +1,5 @@
 #include "parameter_acceptor.h"
-#include <cxxabi.h>
-
-namespace {
-  
-  struct handle {
-    char* p;
-    handle(char* ptr) : p(ptr) { }
-    ~handle() { std::free(p); }
-  };
-  
-  std::string demangle(const char* name) {
-    int status = -4; // some arbitrary value to eliminate the compiler warning
-    handle result( abi::__cxa_demangle(name, NULL, NULL, &status) );
-    return (status==0) ? result.p : name ;
-  };
-  
-  template <class T>
-  std::string type(const T& t) {
-    return demangle(typeid(t).name());
-  };
-
-}
+#include "utilities.h"
 
 
 // Static empty class list
