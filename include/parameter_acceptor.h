910d985338a4b191b008fe9c3e818e8986f24140
Luca Heltai <luca.heltai@sissa.it>
2015-03-28 14:57:50 +0100

    Fixed parameter acceptor.

--- include/parameter_acceptor.h
+++ include/parameter_acceptor.h
@@ -3,70 +3,133 @@
 
 #include <deal.II/base/parameter_handler.h>
 #include <deal.II/base/smartpointer.h>
+#include <boost/any.hpp>
 #include <typeinfo>
 
 using namespace dealii;
 
-/** 
-  A parameter acceptor base class. This is a pure virtual class, used
-  to define a public interface for classes with declare_parameters()
-  and parse_parameters() methods.
-
-  The rationale behind this class, is that we want to make sure all
-  classes declare and parse their own parameters. In order to ensure
-  this, the base class keeps track of pointers to derived classes, and
-  two functions are available which collectively call each declare and
-  parse parameter function.
-*/
+/**
+ * A parameter acceptor base class. This class is used to define a
+ * public interface for classes wich use ParameterHandler to obtain
+ * class parameters. The only function that derived classes should
+ * overload is declare_parameters(). Derived classes are required to
+ * use the add_parameter() function inside the declare_paramters()
+ * function. If they do so, then
+ * ParameterAcceptor::parse_all_parameters() will automatically
+ * populate the variables with the parsed parameters. Failure to use
+ * the add_parameter() function will result in the user having to call
+ * ParameterHandler::get() functions to populate the variables
+ * manually.
+ *
+ * The rationale behind this class, is that we want every class to be
+ * able to declare parameters only once, and then automatically
+ * populate variables with the values from the parameter file, without
+ * having to do so manually.
+ */
 class ParameterAcceptor : public Subscriptor
 {
 public:
-  /** The constructor adds this class to the list of acceptors. If a
-      section name is specified, then this is used to scope the
-      parameters in the given section, otherwise a pretty printed
-      version of the derived class is used. */
-  ParameterAcceptor(const std::string section_name="");
+    /**
+     * The constructor adds this class to the list of acceptors. If a
+     * section name is specified, then this is used to scope the
+     * parameters in the given section, otherwise a pretty printed
+     * version of the derived class is used.
+     */
+    ParameterAcceptor(const std::string section_name="");
 
-  /** The destructor sets to zero the pointer relative to this index,
-      so that it is safe to destroy the mother class. */
-  ~ParameterAcceptor();
-  
-  /** Parse the parameter file. Derived classes need to overload
-      this one. */
-  virtual void parse_parameters(ParameterHandler &prm) = 0;
+    /**
+     * The destructor sets to zero the pointer relative to this index,
+     * so that it is safe to destroy the mother class.
+     */
+    ~ParameterAcceptor();
 
-  /** Generate entries in the given parameter file. Derived classes
-      need to overload this one. */
-  virtual void declare_parameters(ParameterHandler &prm) = 0;
+    /**
+     * Parse the parameter file. This function enters the subsection
+     * returned by get_section_name() for each derived class, and parse
+     * all parameters that were added using add_parameter().
+     */
+    virtual void parse_parameters(ParameterHandler &prm);
 
-  /** Call each of the parse_parameters in the class_list vector. */
-  void parse_all_parameters(ParameterHandler &prm);
-  
-  /** Call each of the declare_parameters in the class_list vector. */
-  void declare_all_parameters(ParameterHandler &prm);
+    /**
+     * Parse parameter call back. This function is called at the end
+     * of parse_parameters, to allow users to process their parameters
+     * right after they have been parsed. The default implementation
+     * is empty.
+     *
+     * You can use this function, for example, to create a quadrature
+     * rule after you have read how many quadrature points you wanted
+     * to use from the parameter file.
+     */
+    virtual void parse_parameters_call_back();
 
-  /** Return the section name of this class. If a name was provided at
-      construction time, then that name is returned, otherwise it
-      returns the name of this class, pretty printed. */
-  std::string get_section_name() const;
 
-private: 
-  /**
-   * A list containing all constructed classes of type
-   * ParameterAcceptor.
-   */
-  static std::vector<SmartPointer<ParameterAcceptor> > class_list;
-  
-  /**
-   * A list containing all section names for the parameter handler.
-   */
-  static std::vector<std::string> section_names;
+    /**
+     * Generate entries in the given parameter file. Derived classes
+     * need to overload this one. If you want to make sure the
+     * automatic assignement of variables work, you should fill this
+     * function only with calls to the add_parameter() method.
+     */
+    virtual void declare_parameters(ParameterHandler &prm) = 0;
 
-  /** The index of this specific class within the class list. */
-  const unsigned int acceptor_id;    
+
+    /**
+     * Parse the given ParameterHandler. This function enters the
+     * subsection returned by get_section_name() for each derived class,
+     * and parses all parameters that were added using add_parameter().
+     */
+    static void parse_all_parameters(ParameterHandler &prm);
+
+
+    /**
+     * Initialize the ParameterHandler with all derived classes
+     * parameters.This function enters the subsection returned by
+     * get_section_name() for each derived class, and declares all
+     * parameters that were added using add_parameter().
+     */
+    static void declare_all_parameters(ParameterHandler &prm);
+
+
+    /**
+     * Return the section name of this class. If a name was provided
+     * at construction time, then that name is returned, otherwise it
+     * returns the name of this class, pretty printed.
+     */
+    std::string get_section_name() const;
+
+
+    /**
+     * Add a parameter the given parameter list. A pointer to the
+     * parameter is stored, so that every time the default
+     * parse_parameters() function is called, this parameter is
+     * updated with the value contained in prm.
+     */
+    template <class T>
+    void add_parameter(ParameterHandler &prm, T *parameter,
+                       const std::string &entry, const std::string &default_value,
+                       const Patterns::PatternBase &pattern=Patterns::Anything(),
+                       const std::string &documentation=std::string()) {
+        prm.declare_entry(entry, default_value, pattern, documentation);
+        parameters[entry] = boost::any(parameter);
+    }
+
+private:
+    /**
+     * A list containing all constructed classes of type
+     * ParameterAcceptor.
+     */
+    static std::vector<SmartPointer<ParameterAcceptor> > class_list;
+
+    /** The index of this specific class within the class list. */
+    const unsigned int acceptor_id;
+
+    /**
+     * A map of parameters that are initialized in this class with the
+     * function add_parameter.
+     */
+    std::map<std::string, boost::any> parameters;
 
 protected:
-  /** The subsection name for this class. */
-  const std::string section_name;
+    /** The subsection name for this class. */
+    const std::string section_name;
 };
 #endif
--- source/parameter_acceptor.cc
+++ source/parameter_acceptor.cc
@@ -4,39 +4,61 @@
 
 // Static empty class list
 std::vector<SmartPointer<ParameterAcceptor> > ParameterAcceptor::class_list;
-std::vector<std::string> ParameterAcceptor::section_names;
-
 
 ParameterAcceptor::ParameterAcceptor(const std::string name) :
-  acceptor_id(class_list.size()),
-  section_name(name)
+    acceptor_id(class_list.size()),
+    section_name(name)
 {
-  SmartPointer<ParameterAcceptor> pt(this, type(*this).c_str());
-  class_list.push_back(pt);
+    SmartPointer<ParameterAcceptor> pt(this, type(*this).c_str());
+    class_list.push_back(pt);
 }
-  
+
 
 ParameterAcceptor::~ParameterAcceptor() {
-  class_list[acceptor_id] = 0;
+    class_list[acceptor_id] = 0;
 }
 
 std::string ParameterAcceptor::get_section_name() const {
-  return (section_name != "" ? section_name : type(*this));
+    return (section_name != "" ? section_name : type(*this));
 }
 
 
 void ParameterAcceptor::parse_all_parameters(ParameterHandler &prm) {
-  for(unsigned int i=0; i< class_list.size(); ++i) {
-    prm.enter_subsection(class_list[i]->get_section_name());
-    class_list[i]->parse_parameters(prm);
-    prm.leave_subsection();
-  }
+    for(unsigned int i=0; i< class_list.size(); ++i) {
+        prm.enter_subsection(class_list[i]->get_section_name());
+        class_list[i]->parse_parameters(prm);
+        prm.leave_subsection();
+    }
 }
 
 void ParameterAcceptor::declare_all_parameters(ParameterHandler &prm) {
-  for(unsigned int i=0; i< class_list.size(); ++i) {
-    prm.enter_subsection(class_list[i]->get_section_name());
-    class_list[i]->declare_parameters(prm);
-    prm.leave_subsection();
-  }
+    for(unsigned int i=0; i< class_list.size(); ++i) {
+        prm.enter_subsection(class_list[i]->get_section_name());
+        class_list[i]->declare_parameters(prm);
+        prm.leave_subsection();
+    }
 }
+
+void ParameterAcceptor::parse_parameters(ParameterHandler &prm) {
+    for(auto it = parameters.begin(); it != parameters.end(); ++it) {
+        if(it->second.type() == typeid(std::string *)) {
+            *(boost::any_cast<std::string*>(it->second)) = prm.get(it->first);
+        }
+        if(it->second.type() == typeid(double *)) {
+            *(boost::any_cast<double*>(it->second)) = prm.get_double(it->first);
+        }
+        if(it->second.type() == typeid(int *)) {
+            *(boost::any_cast<int*>(it->second)) = prm.get_integer(it->first);
+        }
+        if(it->second.type() == typeid(unsigned int *)) {
+            *(boost::any_cast<unsigned int*>(it->second)) = prm.get_integer(it->first);
+        }
+        if(it->second.type() == typeid(bool *)) {
+            *(boost::any_cast<bool*>(it->second)) = prm.get_bool(it->first);
+        }
+    }
+}
+
+
+void ParameterAcceptor::parse_parameters_call_back() {};
+
