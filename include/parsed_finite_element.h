e177fb911b87e0515f51a7e033517e9e42791b7e
Luca Heltai <luca.heltai@sissa.it>
2015-03-29 22:23:25 +0200

    Updated documentation.

--- include/parsed_finite_element.h
+++ include/parsed_finite_element.h
@@ -10,19 +10,70 @@ using namespace dealii;
 
 /**
  * Parsed FiniteElement. Read from a parameter file the name of a
- * finite element, and generate a pointer to it.
+ * finite element, generate one for you, and return a pointer to it.
+ *
+ * The name must be in the form which is returned by the
+ * FiniteElement::get_name function, where dimension template
+ * parameters <2> etc. can be omitted. Alternatively, the explicit
+ * number can be replaced by dim or d. If a number is given, it must
+ * match the template parameter of this function.
+ *
+ * The names of FESystem elements follow the pattern
+ * FESystem[FE_Base1^p1-FE_Base2^p2] The powers p1 etc. may either be
+ * numbers or can be replaced by dim or d.
+ *
+ * If no finite element can be reconstructed from this string, an
+ * exception of type FETools::ExcInvalidFEName is thrown.
+ *
+ * The operator() returns a pointer to a newly create finite element. It
+ * is in the caller's responsibility to destroy the object pointed to
+ * at an appropriate later time.
+ *
+ * Since the value of the template argument can't be deduced from the
+ * (string) argument given to this function, you have to explicitly
+ * specify it when you call this function.
+ *
+ * This function knows about all the standard elements defined in the
+ * library. However, it doesn't by default know about elements that
+ * you may have defined in your program. To make your own elements
+ * known to this function, use the add_fe_name() function.
  */
 template <int dim, int spacedim>
 class ParsedFiniteElement : public ParameterAcceptor
 {
 public:
     /**
-     * Constructor. Takes a name for the section of the Parameter Handler to use.
+     * Constructor. Takes a name for the section of the Parameter
+     * Handler to use.
+     *
+     * This class is derived from ParameterAcceptor. Once you
+     * constructed an object of this class, if you call
+     * ParameterAcceptor::parse_all_parameters(prm), also the
+     * parameters of this class will be filled with values from the
+     * argument ParameterHandler.
      */
     ParsedFiniteElement (std::string name="");
 
     /**
      * Declare possible parameters of this class.
+     *
+     * The only parameter which is declared here is "Finite element
+     * space". The parameter must be in the form which is returned by
+     * the FiniteElement::get_name function, where dimension template
+     * parameters <2> etc. can be omitted. Alternatively, the explicit
+     * number can be replaced by dim or d. If a number is given, it
+     * must match the template parameter of this function.
+     *
+     * The names of FESystem elements follow the pattern
+     * FESystem[FE_Base1^p1-FE_Base2^p2] The powers p1 etc. may either be
+     * numbers or can be replaced by dim or d.
+     *
+     * If no finite element can be reconstructed from this string, an
+     * exception of type FETools::ExcInvalidFEName is thrown.
+     *
+     * The operator() returns a pointer to a newly create finite element. It
+     * is in the caller's responsibility to destroy the object pointed to
+     * at an appropriate later time.
      */
     virtual void declare_parameters(ParameterHandler &prm);
 
