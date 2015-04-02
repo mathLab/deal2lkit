762de920f4e5039c2e8256fbcf5bbbb89438b637
Nicola Giuliani <ngiuliani@sissa.it>
2015-04-02 11:28:16 +0200

    ok the test on the parsed grid generator

--- tests/parsed_grid_generator_02.cc
+++ tests/parsed_grid_generator_02.cc
@@ -20,7 +20,7 @@
 template<int dim, int spacedim>
 void test(ParsedGridGenerator<dim, spacedim> &pgg) {
   auto tria = pgg.serial();
-  *tria.refine_global(3);
+  tria->refine_global(3);
   GridOut go;
   go.write_msh(*tria, deallog.get_file_stream());
   std::ofstream ofile(("/tmp/mesh_"+Utilities::int_to_string(dim)
