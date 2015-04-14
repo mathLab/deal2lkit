66480b29dba07350ebe0ac7a5f9a53ca9cf1b8b6
Luca Heltai <luca.heltai@sissa.it>
2015-04-14 10:15:23 +0200

    Added test for list of int.

---
+++ tests/parameter_acceptor_05.cc
@@ -0,0 +1,47 @@
+//-----------------------------------------------------------
+//
+//    Copyright (C) 2014 by the deal.II authors
+//
+//    This file is subject to LGPL and may not be distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-----------------------------------------------------------
+
+
+#include "tests.h"
+#include "utilities.h"
+#include "parameter_acceptor.h"
+
+template<int dim>
+class Test : public ParameterAcceptor
+{
+public:
+  virtual void declare_parameters(ParameterHandler &prm)
+  {
+    add_parameter(prm, &int_list, "A list of ints", "1, 2", Patterns::List(Patterns::Integer()));
+  };
+
+  void log_info()
+  {
+    deallog << "My type: " << type(*this) << std::endl;
+    for (unsigned int i=0; i<int_list.size(); ++i)
+      deallog << int_list[i] << std::endl;
+  };
+
+private:
+  std::vector<unsigned int> int_list;
+};
+
+
+int main ()
+{
+  initlog();
+  Test<1> a;
+
+  ParameterAcceptor::initialize();
+  ParameterAcceptor::prm.log_parameters(deallog);
+
+  a.log_info();
+}
