975630505b223f963580879e6d593939ecdde8bf
Luca Heltai <luca.heltai@sissa.it>
2015-05-12 21:18:51 +0200

    Added a few functionalities to ParsedFE.

--- include/parsed_finite_element.h
+++ include/parsed_finite_element.h
@@ -5,6 +5,7 @@
 #include <deal.II/fe/fe.h>
 
 #include "parameter_acceptor.h"
+#include "utilities.h"
 
 using namespace dealii;
 
@@ -38,7 +39,7 @@ using namespace dealii;
  * you may have defined in your program. To make your own elements
  * known to this function, use the add_fe_name() function.
  */
-template <int dim, int spacedim>
+template <int dim, int spacedim=dim>
 class ParsedFiniteElement : public ParameterAcceptor
 {
 public:
@@ -51,9 +52,19 @@ public:
    * ParameterAcceptor::parse_all_parameters(prm), also the
    * parameters of this class will be filled with values from the
    * argument ParameterHandler.
+   *
+   * The optional parameters specify the FiniteElement name, the
+   * component names, and the allowed number of components. This class
+   * will throw an exception if the number of components is set to
+   * something different than zero and the corresponding FiniteElement
+   * does not match this number of components. If n_components is left
+   * to 0 (the default value), then any FiniteElement can be
+   * generated, with arbitrary numbers of components.
    */
   ParsedFiniteElement (const std::string &name="",
-                       const std::string &default_fe="FE_Q(1)");
+                       const std::string &default_fe="FE_Q(1)",
+                       const std::string &default_component_names="u",
+                       const unsigned int n_components=0);
 
   /**
    * Declare possible parameters of this class.
@@ -79,16 +90,90 @@ public:
   virtual void declare_parameters(ParameterHandler &prm);
 
   /**
-   * Return a pointer to a newly created Finite Element. It will throw
-   * an exception if called before any parsing has occured.
+   * Return a shared pointer to a newly created Finite Element. It
+   * will throw an exception if called before any parsing has occured,
+   * or if the number of components of the generated finite element is
+   * different from the number of components given at construction
+   * time.
    */
   FiniteElement<dim, spacedim> *operator() ();
 
+  /**
+   * Fill information about blocks after parsing the parameters.
+   */
+  virtual void parse_parameters_call_back();
+
+  /**
+   * Return the component names for this Finite Element.
+   */
+  std::string get_component_names() const;
+
+  /**
+   * Return the blocking of the components for this finite
+   * element. This is what's needed by the block renumbering
+   * algorithm.
+   */
+  std::vector<unsigned int> get_component_blocks() const;
+
+
+  /**
+   * Return the block names for this Finite Element. This is the same
+   * as std::unique(get_component_names().begin(),
+   * get_component_names().end())
+   */
+  std::string get_block_names() const;
+
+  /**
+   * Return the number of blocks of the Finite Element.
+   */
+  unsigned int n_components() const;
+
+  /**
+   * Return the number of blocks of the Finite Element.
+   */
+  unsigned int n_blocks() const;
+
 private:
+  /**
+   * Number of components of this FiniteElement. If you want to allow
+   * for arbitrary components, leave this to its default value 0.
+   */
+  const unsigned int _n_components;
+
   /**
    * Finite Element Name.
    */
   std::string fe_name;
+
+  /**
+   * Default component names.
+   */
+  std::string default_component_names;
+
+  /**
+   * Block names. This is comma separeted list of component names
+   * which identifies the Finite Elemenet. If a name is repeated, then
+   * that component is assumed to be part of a vector field or Tensor
+   * field, and is treated as a single block. User classes can use
+   * this information to construct block matrices and vectors, or to
+   * group solution names according to components. For example, a
+   * Stokes problem may have "u,u,p" for dim = 2 or "u, u, u, p" for
+   * dim = 3.
+   */
+  std::vector<std::string> component_names;
+
+  /**
+   * The subdivision, in terms of component indices. This is
+   * automatically computed from the the block names.
+   */
+  std::vector<unsigned int> component_blocks;
+
+
+  /**
+   * The subdivision, in terms of component indices. This is
+   * automatically computed from the the block names.
+   */
+  std::vector<std::string> block_names;
 };
 
 #endif
