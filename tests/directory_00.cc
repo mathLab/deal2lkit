2f3795cfbd35f24fc9f8be87eecbc37751a7c141
ESeNonFossiIo <esenonfossiio@gmail.com>
2015-05-18 21:02:50 +0200

    indentation

--- tests/directory_00.cc
+++ tests/directory_00.cc
@@ -19,24 +19,24 @@ int main ()
   std::string cmd = "";
 
   cmd = name_dir;
-  if(std::system(cmd.c_str()))
-  {
-    cmd = "test -d "+name_dir;
-    if(std::system(cmd.c_str()))
-      deallog << "The directory " + name_dir + " has been correctly created." << std::endl;
+  if (std::system(cmd.c_str()))
+    {
+      cmd = "test -d "+name_dir;
+      if (std::system(cmd.c_str()))
+        deallog << "The directory " + name_dir + " has been correctly created." << std::endl;
 
-    cmd = "rmdir" + name_dir;
-    if(std::system(cmd.c_str()))
-    {
-      deallog << "The directory " + name_dir + " has been correctly removed." << std::endl;
+      cmd = "rmdir" + name_dir;
+      if (std::system(cmd.c_str()))
+        {
+          deallog << "The directory " + name_dir + " has been correctly removed." << std::endl;
+        }
+      else
+        {
+          deallog << "Impossible to remove " + name_dir + "." << std::endl;
+        }
     }
-    else
-    {
-      deallog << "Impossible to remove " + name_dir + "." << std::endl;
-    }
-  }
   else
-  {
-    deallog << "Impossible to create " + name_dir + "." << std::endl;
-  }
+    {
+      deallog << "Impossible to create " + name_dir + "." << std::endl;
+    }
 }
