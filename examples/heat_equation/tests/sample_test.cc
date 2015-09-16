cc03d739e9371b9f3e70f805cc4a99b09ba36bd2
alberto sartori <alberto.sartori86@gmail.com>
2015-09-16 17:11:51 +0200

    first example

---
+++ examples/heat_equation/tests/sample_test.cc
@@ -0,0 +1,13 @@
+#include "heat_ida.h"
+#include "tests.h"
+
+using namespace dealii;
+int main (int argc, char *argv[])
+{
+
+  initlog();
+
+	deallog << "sample test" << std::endl;
+
+  return 0;
+}
