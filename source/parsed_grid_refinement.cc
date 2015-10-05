07eb0fa0cf12a63e34933679a8f2caea21a95495
Luca Heltai <luca.heltai@sissa.it>
2015-10-05 19:29:06 +0200

    Added parsed grid refinement.

---
+++ source/parsed_grid_refinement.cc
@@ -0,0 +1,48 @@
+#include <deal2lkit/parsed_grid_refinement.h>
+
+
+#ifdef DEAL_II_WITH_CXX11
+
+ParsedGridRefinement::ParsedGridRefinement(const std::string &name,
+					   const std::string &strategy,
+					   const double &top_parameter,
+					   const double &bottom_parameter,
+					   const unsigned int &max_cells,
+					   const unsigned int &order) :
+  ParameterAcceptor(name), 
+  strategy(strategy),
+  top_parameter(top_parameter),
+  bottom_parameter(bottom_parameter),
+  order(order)
+{}
+
+void ParsedGridRefinement::declare_parameters(ParameterHandler &prm) {
+  add_parameter(prm, &strategy,
+                "Refinement strategy", strategy,
+                Patterns::Selection("fraction|number"),
+                "Refinement strategy to use. fraction|number");
+  
+  add_parameter(prm, &top_parameter,
+                "Top fraction", std::to_string(top_parameter),
+                Patterns::Double(0.0),
+		"Refinement fraction.");
+  
+  add_parameter(prm, &bottom_parameter,
+                "Bottom fraction", std::to_string(bottom_parameter),
+                Patterns::Double(0.0),
+		"Coarsening fraction.");
+
+  add_parameter(prm, &max_cells,
+                "Maximum number of cells (if available)", std::to_string(max_cells),
+                Patterns::Integer(0),
+		"Maximum number of cells.");
+
+  
+  add_parameter(prm, &order,
+                "Order (optimize)", std::to_string(order),
+                Patterns::Integer(0),
+		"Maximum number of cells.");
+
+}
+
+#endif
---
+++ tests/parsed_grid_refinement_02.cc
@@ -0,0 +1,62 @@
+//-----------------------------------------------------------
+//
+//    Copyright (C) 2014 by the deal.II authors
+//
+//    This file is subject to LGPL and may not be distributed
+//    without copyright and license information. Please refer
+//    to the file deal.II/doc/license.html for the  text  and
+//    further information on this license.
+//
+//-----------------------------------------------------------
+
+
+#include "tests.h"
+#include <deal2lkit/utilities.h>
+#include <deal2lkit/parsed_grid_generator.h>
+#include <deal2lkit/parsed_grid_refinement.h>
+
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/base/utilities.h>
+
+#include <deal.II/lac/vector.h>
+
+template<int dim, int spacedim>
+void test()
+{
+  ParsedGridGenerator<dim, spacedim> pgg;
+  ParsedGridRefinement pgr("", "number");
+  
+  ParameterAcceptor::initialize();
+  
+  Triangulation<dim, spacedim> *tria = pgg.serial();
+  
+  tria->refine_global(3);
+
+  Vector<float> criteria(tria->n_active_cells());
+  
+  for(auto cell : tria->active_cell_iterators())
+    criteria[cell->index()] = cell->center().norm();
+
+  pgr.mark_cells(criteria, *tria);
+
+  tria->execute_coarsening_and_refinement();
+  
+  GridOut go;
+  go.write_msh(*tria, deallog.get_file_stream());
+  std::ofstream ofile(("/tmp/mesh_"+Utilities::int_to_string(dim)
+                       +Utilities::int_to_string(spacedim)+".msh").c_str());
+  go.write_msh(*tria, ofile);
+  
+  delete tria;
+}
+
+int main ()
+{
+  initlog();
+
+  test<1,1>();
+  test<1,2>();
+  test<2,2>();
+  test<2,3>();
+  test<3,3>();
+}
