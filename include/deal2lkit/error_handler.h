b22f8a6bf6f242798df89d190738d26a5131ded8
alberto sartori <alberto.sartori86@gmail.com>
2015-10-08 17:39:01 +0200

    fixed header and copyright

--- include/deal2lkit/error_handler.h
+++ include/deal2lkit/error_handler.h
@@ -1,5 +1,20 @@
-#ifndef LH_ERROR_HANDLER_H
-#define LH_ERROR_HANDLER_H
+//-----------------------------------------------------------
+//
+//    Copyright (C) 2014 - 2015 by the deal2lkit authors
+//
+//    This file is part of the deal2lkit library.
+//
+//    The deal2lkit library is free software; you can use it, redistribute
+//    it, and/or modify it under the terms of the GNU Lesser General
+//    Public License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//    The full text of the license can be found in the file LICENSE at
+//    the top level of the deal2lkit distribution.
+//
+//-----------------------------------------------------------
+
+#ifndef _d2k_error_handler_h
+#define _d2k_error_handler_h
 
 #include <deal2lkit/config.h>
 #include <fstream>
--- include/deal2lkit/sundials_interface.h
+++ include/deal2lkit/sundials_interface.h
@@ -1,5 +1,20 @@
-#ifndef __sak__sundials_interface_h
-#define __sak__sundials_interface_h
+//-----------------------------------------------------------
+//
+//    Copyright (C) 2015 by the deal2lkit authors
+//
+//    This file is part of the deal2lkit library.
+//
+//    The deal2lkit library is free software; you can use it, redistribute
+//    it, and/or modify it under the terms of the GNU Lesser General
+//    Public License as published by the Free Software Foundation; either
+//    version 2.1 of the License, or (at your option) any later version.
+//    The full text of the license can be found in the file LICENSE at
+//    the top level of the deal2lkit distribution.
+//
+//-----------------------------------------------------------
+
+#ifndef _d2k_sundials_interface_h
+#define _d2k_sundials_interface_h
 
 #include <deal2lkit/config.h>
 #include <deal2lkit/utilities.h>
