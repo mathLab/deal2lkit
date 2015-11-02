e8ecc34d2eddff2f5af61f97e46e4c11c2660ad1
alberto sartori <alberto.sartori86@gmail.com>
2015-11-02 17:13:03 +0100

    indent also examples

--- examples/assimp_importer/assimp_importer.cc
+++ examples/assimp_importer/assimp_importer.cc
@@ -27,24 +27,32 @@ using namespace deal2lkit;
 // A minimal program that takes as input a file in a format supported
 // by Assimp, and generates a grid supported by deal.II
 
-int main (int argc, char ** argv)
+int main (int argc, char **argv)
 {
   AssertThrow(argc == 3, ExcMessage("Must specify exactly 2 arguments!"));
   Triangulation<2,3> tria;
-  if(AssimpInterface::generate_triangulation(argv[1], tria, -1, true, 1e-3)) {
-    GridOut grid_out;
-    std::string fname(argv[2]);
-    std::ofstream out (argv[2]);
-    
-    AssertThrow(out, ExcIO());
-    if(fname.find("msh") !=  std::string::npos) {
-      grid_out.write_msh (tria, out);
-    } else if(fname.find("vtk") !=  std::string::npos) {
-      grid_out.write_vtk (tria, out);
-    } else if(fname.find("inp") != std::string::npos) {
-      grid_out.write_ucd (tria, out);
-    } else {
-      AssertThrow(false, ExcMessage("Unrecognized output format. vtk, msh or inp."));
+  if (AssimpInterface::generate_triangulation(argv[1], tria, -1, true, 1e-3))
+    {
+      GridOut grid_out;
+      std::string fname(argv[2]);
+      std::ofstream out (argv[2]);
+
+      AssertThrow(out, ExcIO());
+      if (fname.find("msh") !=  std::string::npos)
+        {
+          grid_out.write_msh (tria, out);
+        }
+      else if (fname.find("vtk") !=  std::string::npos)
+        {
+          grid_out.write_vtk (tria, out);
+        }
+      else if (fname.find("inp") != std::string::npos)
+        {
+          grid_out.write_ucd (tria, out);
+        }
+      else
+        {
+          AssertThrow(false, ExcMessage("Unrecognized output format. vtk, msh or inp."));
+        }
     }
-  }
 }
