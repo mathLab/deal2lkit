d53379f9aae1a5b7e4a0e9aa14dea826763d2ee8
Luca Heltai <luca.heltai@sissa.it>
2015-11-30 14:46:07 +0100

    Fixed stokes ida.

--- examples/dynamic_stokes/source/stokes_ida.cc
+++ examples/dynamic_stokes/source/stokes_ida.cc
@@ -615,7 +615,7 @@ void Stokes<dim>::output_step(const double t,
     }
   data_out.add_data_vector (distributed_solution_dot, print(sol_dot_names,","));
 
-  data_out.write_data_and_clear("",*mapping);
+  data_out.write_data_and_clear(*mapping);
 
   computing_timer.exit_section ();
 }
