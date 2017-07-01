d8e8ec3d2f46521e73474829a58dfb31f5107726
Luca Heltai <luca.heltai@sissa.it>
2017-07-01 10:43:25 +0200

    Mount as root.

--- scripts/dr.sh
+++ scripts/dr.sh
@@ -1,1 +1,1 @@
-docker run --privileged -t --rm -P -v `pwd`:/home/dealii/deal2lkit:rw mathlab/deal2lkit-base:v8.5.0 /bin/sh -c "$@"
+docker run --user root -t --rm -P -v `pwd`:/home/dealii/deal2lkit:rw mathlab/deal2lkit-base:v8.5.0 /bin/sh -c "$@"
