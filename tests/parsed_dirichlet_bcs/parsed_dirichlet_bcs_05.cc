adf64aa2e67ac39b73ba5c7447395409d76bfec9
Alberto Sartori <alberto.sartori86@gmail.com>
2017-10-13 11:09:08 +0200

    removed old command

--- tests/parsed_dirichlet_bcs/parsed_dirichlet_bcs_05.cc
+++ tests/parsed_dirichlet_bcs/parsed_dirichlet_bcs_05.cc
@@ -97,7 +97,6 @@ int main()
   deallog << std::setprecision (2);
   deallog << std::fixed;
   deallog.depth_console (0);
-  deallog.threshold_double(1.e-12);
   //ParameterAcceptor::prm.log_parameters(deallog);
 
   test_hyper_cube<2>();
