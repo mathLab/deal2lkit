cb190fcacb1b3e263e28932c91551adaf12868b6
Luca Heltai <luca.heltai@sissa.it>
2019-08-07 02:46:18 -0600

    Replace deprecated headers.

--- tests/parsed_dirichlet_bcs/parsed_dirichlet_bcs_04.cc
+++ tests/parsed_dirichlet_bcs/parsed_dirichlet_bcs_04.cc
@@ -29,9 +29,9 @@
 
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_refinement.h>
+#include <deal.II/grid/manifold_lib.h>
 #include <deal.II/grid/tria.h>
 #include <deal.II/grid/tria_accessor.h>
-#include <deal.II/grid/tria_boundary_lib.h>
 #include <deal.II/grid/tria_iterator.h>
 
 #include <deal.II/lac/vector.h>
