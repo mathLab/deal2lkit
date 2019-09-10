5cad2fd65194719ee548f5215ef4bc9511adb56f
Luca Heltai <luca.heltai@sissa.it>
2019-09-10 22:08:25 +0200

    Fixed permissions.

--- scripts/dr.sh
+++ scripts/dr.sh
@@ -1,1 +1,1 @@
-docker run -i -t --rm -P -v `pwd`:/home/dealii/deal2lkit:rw dealii/dealii:v9.1.1-gcc-mpi-fulldepsspack-debugrelease /bin/sh -c "cd deal2lkit; $@"
+docker run --user $(id -u):$(id -g) -i -t --rm -P -v `pwd`:/home/dealii/deal2lkit:rw dealii/dealii:v9.1.1-gcc-mpi-fulldepsspack-debugrelease /bin/sh -c "cd deal2lkit; $@"
